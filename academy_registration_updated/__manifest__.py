522e72a54018faee624d5d3be5980d87341f73aa
MHD Manar Zamrik <manar.z@ft.iq>
2025-06-02 14:26:19 +0300

    Add files via upload

---
+++ academy_registration_updated/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Academy Player Registration",
+    "version": "1.3",
+    "summary": "ربط استمارة تسجيل اللاعبين بجهات الاتصال في أودو",
+    "category": "Uncategorized",
+    "author": "Your Company",
+    "depends": ["base", "contacts"],
+    "data": [
+        "views/player_registration_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3",
+}
